f7efffcdce4ebb4d3d3f695bf4ff795488faf550
olasharaff <sharafabdulrahman@icloud.com>
2023-07-07 08:37:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+let image = document.querySelectorAll('img');
+
+image.forEach((e) => {
+    e.addEventListener('click', ()=>{
+        const altText = e.alt;
+        alert(`You clicked on ${altText}`);
+    })
+})
+
+const primaryNav = document.querySelector('.primary-navigation');
+const toggleButton = document.querySelector('.mobile-toggle');
+
+toggleButton.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    const expanded = toggleButton.getAttribute('data-expanded');
+
+    if(visibility === 'false') {
+        primaryNav.setAttribute('data-visible', true)
+        toggleButton.setAttribute('aria-expanded',true);
+       
+        
+    } else if(visibility === 'true') {
+       primaryNav.setAttribute('data-visible', false);
+        toggleButton.setAttribute('aria-expanded', false)
+    
+    }
+
+   
+})
